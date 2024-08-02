7927e539e90cfedd3edafb322886a22e74de998e
Rado. G <rzgeorgiev@outlook.com>
2024-08-02 23:58:32 +0300

    Adding forgotten password page

--- Client/MotoDev/src/components/app/app.routes.ts
+++ Client/MotoDev/src/components/app/app.routes.ts
@@ -4,10 +4,12 @@ import { RegisterComponent } from '../register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AuthComponent } from '../auth/auth.component';
+import { ForgottenPasswordComponent } from '../forgotten-password/forgotten-password.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'forgottenPassword', component: ForgottenPasswordComponent },
     { path: '',  component: AuthComponent }
 ];
 
--- Client/MotoDev/src/components/register/register.component.ts
+++ Client/MotoDev/src/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, RequiredValidator, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router, RouterModule } from '@angular/router';
 import { AppRoutingModule, routes } from '../app/app.routes';
@@ -7,20 +7,21 @@ import { AppRoutingModule, routes } from '../app/app.routes';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   
-  registerForm: FormGroup | undefined;
+  registerForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
      private authService: AuthService,
      private router: Router) {
       this.registerForm = this.formBuilder.group({
-        username: ['', Validators.required],
+        name: ['', Validators.required],
         email: ['', Validators.required],
+        username: ['', Validators.required],
         password: ['', [Validators.required, Validators.minLength(8)]]
       });
   }
