5c68be28f8f25c9caad771f1621c87d4811cf43b
Rado. G <rzgeorgiev@outlook.com>
2024-08-03 16:48:12 +0300

    Finished register page design

--- Client/MotoDev/src/components/register/register.component.ts
+++ Client/MotoDev/src/components/register/register.component.ts
@@ -1,32 +1,40 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormGroup, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router, RouterModule } from '@angular/router';
 import { AppRoutingModule, routes } from '../app/app.routes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   
   registerForm!: FormGroup;
+  isSubmitted: boolean = false;
 
   constructor(private formBuilder: FormBuilder,
-     private authService: AuthService,
-     private router: Router) {
-      this.registerForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        email: ['', Validators.required],
-        username: ['', Validators.required],
-        password: ['', [Validators.required, Validators.minLength(8)]]
-      });
+    private authService: AuthService,
+    private router: Router) {
+ }
+ 
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      acceptTerms: [false, Validators.required]
+    });
   }
+ 
 
   onSubmit() {
+    this.isSubmitted = true;
     if (this.registerForm?.valid) {
       this.authService.register(this.registerForm.value).subscribe(
         response => {
