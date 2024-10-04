79e5f1d3819eb33119020bf1be526c4cc73dea1d
Rado. G <rzgeorgiev@outlook.com>
2024-10-04 14:50:14 +0300

    Fixing repair statuses

--- Client/MotoDev/src/app/shared/consts/urls.ts
+++ Client/MotoDev/src/app/shared/consts/urls.ts
@@ -1,4 +1,4 @@
 export abstract class Urls {
-    private static readonly IsInProd: boolean = false;
+    private static readonly IsInProd: boolean = true;
     static readonly ApiUrl: string = this.IsInProd ? "http://motodev-api.space" : "https://localhost:5078";
 }
